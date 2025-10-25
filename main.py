59e728c4f3d61e9458ca9f44ed222f48e0edbe95
Deepesh Kabra <deepeshkabrawork@gmail.com>
2025-10-26 01:34:37 +0530

    Complete the first prototype

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-# pip install -qU "langchain[anthropic]" to call the model
 import os
-import faiss
-
-from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
-from langchain_community.document_loaders import WebBaseLoader
+from token import GREATER
 from dotenv import load_dotenv
+
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_classic.tools.retriever import create_retriever_tool
+from langgraph.graph import MessagesState
+from langchain_ollama import OllamaEmbeddings
+
+
 load_dotenv()
 
 os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
@@ -12,8 +19,300 @@ os.environ["LANGSMITH_TRACING"] = "true"
 os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
 
 
-model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
-embeddings = GoogleGenerativeAIEmbeddings(model = "models/gemini-embedding-001")
+response_model = ChatGoogleGenerativeAI(model="gemini-2.0-flash-lite")
+embeddings = OllamaEmbeddings(model = "nomic-embed-text:v1.5")
 
-vectorstore = FAISS.from_texts(texts, embeddings)
-# Run the agent
+urls = [
+    "https://lilianweng.github.io/posts/2024-11-28-reward-hacking/",
+    "https://lilianweng.github.io/posts/2024-07-07-hallucination/",
+    "https://lilianweng.github.io/posts/2024-04-12-diffusion-video/",
+]
+
+docs = [WebBaseLoader(url).load() for url in urls]
+docs[0][0].page_content.strip()[:1000] 
+
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=100, chunk_overlap=50)
+
+docs_split = text_splitter.split_documents(docs_list)
+
+docs_split[0].page_content.strip()
+
+vectorstore = InMemoryVectorStore.from_documents(documents=docs_split, embedding=embeddings)
+
+retriever = vectorstore.as_retriever()
+
+retriever_tool = create_retriever_tool(retriever, "retrieve_blog_posts", "Search and return information about Lilian Weng's blog posts.")
+
+retriever_tool.invoke("What are the main challenges discussed in the blog post about reward hacking?")
+
+def generate_query_or_respond(state: MessagesState) : 
+    """Call the model to generate a response based on the current state. Given
+    the question, it will decide to retrieve using the retriever tool, or simply respond to the user.
+    """
+    response = response_model.bind_tools([retriever_tool]).invoke(state["messages"])
+    return {"messages": [response]}
+
+input = {
+    "messages": [
+        {
+            "role": "user",
+            "content": "What does Lilian Weng say about types of reward hacking?",
+        }
+    ]
+}
+generate_query_or_respond(input)["messages"][-1].pretty_print()
+
+# Add a conditional edge — grade_documents — to determine whether the retrieved documents are relevant to the question. We will use a model with a structured output schema GradeDocuments for document grading. The grade_documents function will return the name of the node to go to based on the grading decision (generate_answer or rewrite_question):
+
+from pydantic import BaseModel, Field
+from typing import Literal
+
+GRADE_PROMPT = (
+    "You are a grader assessing relevance of a retrieved document to a user question. \n "
+    "Here is the retrieved document: \n\n {context} \n\n"
+    "Here is the user question: {question} \n"
+    "If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. \n"
+    "Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."
+)
+
+
+class GradeDocuments(BaseModel):  
+    """Grade documents using a binary score for relevance check."""
+
+    binary_score: str = Field(
+        description="Relevance score: 'yes' if relevant, or 'no' if not relevant"
+    )
+
+
+grader_model = ChatGoogleGenerativeAI(model="gemini-2.0-flash-lite")
+
+
+def grade_documents(
+    state: MessagesState,
+) -> Literal["generate_answer", "rewrite_question"]:
+    """Determine whether the retrieved documents are relevant to the question."""
+    question = state["messages"][0].content
+    context = state["messages"][-1].content
+
+    prompt = GRADE_PROMPT.format(question=question, context=context)
+    response = (
+        grader_model
+        .with_structured_output(GradeDocuments).invoke(  
+            [{"role": "user", "content": prompt}]
+        )
+    )
+    score = response.binary_score
+
+    if score == "yes":
+        return "generate_answer"
+    else:
+        return "rewrite_question"
+
+# Run this with irrelevant documents in the tool response:
+
+from langchain_core.messages import convert_to_messages
+
+input = {
+    "messages": convert_to_messages(
+        [
+            {
+                "role": "user",
+                "content": "What does Lilian Weng say about types of reward hacking?",
+            },
+            {
+                "role": "assistant",
+                "content": "",
+                "tool_calls": [
+                    {
+                        "id": "1",
+                        "name": "retrieve_blog_posts",
+                        "args": {"query": "types of reward hacking"},
+                    }
+                ],
+            },
+            {"role": "tool", "content": "meow", "tool_call_id": "1"},
+        ]
+    )
+}
+grade_documents(input)
+
+# Confirm that the relevant documents are classified as such:
+
+input = {
+    "messages": convert_to_messages(
+        [
+            {
+                "role": "user",
+                "content": "What does Lilian Weng say about types of reward hacking?",
+            },
+            {
+                "role": "assistant",
+                "content": "",
+                "tool_calls": [
+                    {
+                        "id": "1",
+                        "name": "retrieve_blog_posts",
+                        "args": {"query": "types of reward hacking"},
+                    }
+                ],
+            },
+            {
+                "role": "tool",
+                "content": "reward hacking can be categorized into two types: environment or goal misspecification, and reward tampering",
+                "tool_call_id": "1",
+            },
+        ]
+    )
+}
+grade_documents(input)
+
+# Build the rewrite_question node. The retriever tool can return potentially irrelevant documents, which indicates a need to improve the original user question. To do so, we will call the rewrite_question node:
+
+REWRITE_PROMPT = (
+    "Look at the input and try to reason about the underlying semantic intent / meaning.\n"
+    "Here is the initial question:"
+    "\n ------- \n"
+    "{question}"
+    "\n ------- \n"
+    "Formulate an improved question:"
+)
+
+
+def rewrite_question(state: MessagesState):
+    """Rewrite the original user question."""
+    messages = state["messages"]
+    question = messages[0].content
+    prompt = REWRITE_PROMPT.format(question=question)
+    response = response_model.invoke([{"role": "user", "content": prompt}])
+    return {"messages": [{"role": "user", "content": response.content}]}
+
+input = {
+    "messages": convert_to_messages(
+        [
+            {
+                "role": "user",
+                "content": "What does Lilian Weng say about types of reward hacking?",
+            },
+            {
+                "role": "assistant",
+                "content": "",
+                "tool_calls": [
+                    {
+                        "id": "1",
+                        "name": "retrieve_blog_posts",
+                        "args": {"query": "types of reward hacking"},
+                    }
+                ],
+            },
+            {"role": "tool", "content": "meow", "tool_call_id": "1"},
+        ]
+    )
+}
+
+response = rewrite_question(input)
+print(response["messages"][-1]["content"])
+
+GENERATE_PROMPT = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer the question. "
+    "If you don't know the answer, just say that you don't know. "
+    "Use three sentences maximum and keep the answer concise.\n"
+    "Question: {question} \n"
+    "Context: {context}"
+)
+
+
+def generate_answer(state: MessagesState):
+    """Generate an answer."""
+    question = state["messages"][0].content
+    context = state["messages"][-1].content
+    prompt = GENERATE_PROMPT.format(question=question, context=context)
+    response = response_model.invoke([{"role": "user", "content": prompt}])
+    return {"messages": [response]}
+input = {
+    "messages": convert_to_messages(
+        [
+            {
+                "role": "user",
+                "content": "What does Lilian Weng say about types of reward hacking?",
+            },
+            {
+                "role": "assistant",
+                "content": "",
+                "tool_calls": [
+                    {
+                        "id": "1",
+                        "name": "retrieve_blog_posts",
+                        "args": {"query": "types of reward hacking"},
+                    }
+                ],
+            },
+            {
+                "role": "tool",
+                "content": "reward hacking can be categorized into two types: environment or goal misspecification, and reward tampering",
+                "tool_call_id": "1",
+            },
+        ]
+    )
+}
+
+response = generate_answer(input)
+response["messages"][-1].pretty_print()
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import ToolNode, tools_condition
+
+workflow = StateGraph(MessagesState)
+
+# Define the nodes we will cycle between
+workflow.add_node(generate_query_or_respond)
+workflow.add_node("retrieve", ToolNode([retriever_tool]))
+workflow.add_node(rewrite_question)
+workflow.add_node(generate_answer)
+
+workflow.add_edge(START, "generate_query_or_respond")
+
+# Decide whether to retrieve
+workflow.add_conditional_edges(
+    "generate_query_or_respond",
+    # Assess LLM decision (call `retriever_tool` tool or respond to the user)
+    tools_condition,
+    {
+        # Translate the condition outputs to nodes in our graph
+        "tools": "retrieve",
+        END: END,
+    },
+)
+
+# Edges taken after the `action` node is called.
+workflow.add_conditional_edges(
+    "retrieve",
+    # Assess agent decision
+    grade_documents,
+)
+workflow.add_edge("generate_answer", END)
+workflow.add_edge("rewrite_question", "generate_query_or_respond")
+
+# Compile
+graph = workflow.compile()
+
+from IPython.display import Image, display
+
+display(Image(graph.get_graph().draw_mermaid_png()))
+
+for chunk in graph.stream(
+    {
+        "messages": [
+            {
+                "role": "user",
+                "content": "What does Lilian Weng say about types of reward hacking?",
+            }
+        ]
+    }
+):
+    for node, update in chunk.items():
+        print("Update from node", node)
+        update["messages"][-1].pretty_print()
+        print("\n\n")
